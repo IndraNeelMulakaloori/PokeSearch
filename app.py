c3805707881c008422919fb3c2da70e31fe87163
IndraNeelMulakaloori <neel16112001@gmail.com>
2024-04-07 23:03:52 +0530

    Type : Update And Add code Desc : Add files pokeapi.py to fetch data from pokemon api and structured in html using flask

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import  Flask,render_template,request
 import requests
+import pokeapi
+
 
 app = Flask(__name__)
 
@@ -9,9 +11,11 @@ def hello_world():
 
 @app.route("/result",methods=["GET", "POST"])
 def result():
-    pokemon = requests.form['name']
-    
-    return render_template('result.html',pokemon = pokemon)
+    pokemon_name = request.form['pokemon']
+
+    response = pokeapi.fetchResult(pokemon_name)
+    print(response['picture'])
+    return render_template('result.html',response = response)
 
 if __name__ == '__main__':
     app.run(port = 8000,debug=True)
